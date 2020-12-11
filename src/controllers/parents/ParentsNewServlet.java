9e02f29ed30c6e3928952148e3d4771961d062fb
hiyuu.akamatsu <hiyu.1211@icloud.com>
2020-12-12 00:03:20 +0900

    Add Parents new

--- src/controllers/parents/ParentsNewServlet.java
+++ src/controllers/parents/ParentsNewServlet.java
@@ -1,19 +1,23 @@
 package controllers.parents;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Parents;
+
 /**
  * Servlet implementation class ParentsNewServlet
  */
 @WebServlet("/parents/new")
 public class ParentsNewServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,12 +26,15 @@ public class ParentsNewServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("_token", request.getSession().getId());
+        request.setAttribute("parents", new Parents());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/parents/new.jsp");
+        rd.forward(request, response);
+    }
 
 }
